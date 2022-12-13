2ee1ee49d2f5904dcd155e87d1de9fd378649006
Alon Shlider <alon.shlider@gmail.com>
2022-12-13 16:10:34 +0200

    dashboard card details outside of image

--- app/src/main/java/com/tinytap/ui/screens/dashboard/state/data/DashboardCard.kt
+++ app/src/main/java/com/tinytap/ui/screens/dashboard/state/data/DashboardCard.kt
@@ -1,6 +1,7 @@
 package com.tinytap.ui.screens.dashboard.state.data
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -9,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -17,58 +20,70 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.tinytap.model.ui_models.DashboardCardModel
+import com.tinytap.ui.theme.backgroundEndColor
+import com.tinytap.ui.theme.backgroundStartColor
 
 @Composable
-fun DashboardCard(model: DashboardCardModel) {
-    Card(
-        modifier = Modifier
-            .size(200.dp)
+fun DashboardCard(
+    modifier: Modifier = Modifier,
+    model: DashboardCardModel
+) {
+    Column(
+        modifier = modifier
+            .size(230.dp, 300.dp)
+            .background(Color.Transparent)
             .padding(16.dp),
-        shape = RoundedCornerShape(30.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            if (model.showDefaultThumbnail) {
-                AsyncImage(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(10.dp),
-                    model = model.thumbnailUrl, contentDescription = ""
-                )
-            } else {
-                Image(
-                    modifier = Modifier.padding(10.dp),
-                    painter = painterResource(id = com.tinytap.R.drawable.tinytap),
-                    contentDescription = ""
-                )
-            }
-
-            Text(
-                modifier = Modifier.padding(10.dp),
-                fontSize = 16.sp,
-                color = Color.Black,
-                text = model.title,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+        if (model.showDefaultThumbnail) {
+            AsyncImage(
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                model = model.thumbnailUrl, contentDescription = ""
             )
-            Text(
-                modifier = Modifier.padding(5.dp),
-                text = model.subtitle,
-                fontSize = 12.sp,
-                color = Color.LightGray,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+        } else {
+            Image(
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                painter = painterResource(id = com.tinytap.R.drawable.tinytap),
+                contentDescription = ""
             )
         }
+        Text(
+            modifier = Modifier.padding(top = 10.dp),
+            fontSize = 16.sp,
+            color = Color.White,
+            text = model.title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = model.subtitle,
+            fontSize = 12.sp,
+            color = Color.LightGray,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DashboardCardPreview() {
-    DashboardCard(DashboardCardModel("image", "TinyTap", "TinyTap Games"))
+    DashboardCard(
+        modifier = Modifier.background(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    backgroundStartColor,
+                    backgroundEndColor
+                )
+            )
+        ),
+        model = DashboardCardModel(
+            "image", "TinyTap", "TinyTap Games"
+        )
+    )
 }
--- app/src/main/java/com/tinytap/ui/screens/dashboard/state/data/DashboardDataState.kt
+++ app/src/main/java/com/tinytap/ui/screens/dashboard/state/data/DashboardDataState.kt
@@ -7,11 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.tinytap.model.ui_models.DashboardCardModel
 import com.tinytap.ui.theme.backgroundEndColor
@@ -34,14 +33,23 @@ fun DashboardDataState(dashboardCardModels: List<DashboardCardModel>) {
     ) {
         LazyRow(modifier = Modifier.wrapContentSize()) {
             items(dashboardCardModels) { model ->
-                DashboardCard(model)
+                DashboardCard(model = model)
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.TABLET, widthDp = 720, heightDp = 360)
 @Composable
 fun DashboardDataStatePreview() {
-    DashboardDataState(emptyList())
+    DashboardDataState(
+        listOf(
+            DashboardCardModel("url", "TinyTap", "TinyTap"),
+            DashboardCardModel("url", "TinyTap2", "TinyTap2"),
+            DashboardCardModel("url", "TinyTap3", "TinyTap3"),
+            DashboardCardModel("url", "TinyTap4", "TinyTap4"),
+            DashboardCardModel("url", "TinyTap5", "TinyTap5"),
+            DashboardCardModel("url", "TinyTap5", "TinyTap5"),
+        )
+    )
 }
